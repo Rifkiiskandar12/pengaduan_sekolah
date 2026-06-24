08306161b7df552f9a3af26b9da07eb719e16277
Rifkiskndr12 <mribstudio@gmail.com>
2026-06-24 09:22:49 +0700

    feat: add private route protection for authenticated pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import FormPengaduan from "./pages/FormPengaduan";
 import ListPengaduan from "./pages/ListPengaduan";
-import MainLayout from "./layouts/MainLayout";
-import Register from "./pages/Register";
 import DetailPengaduan from "./pages/DetailPengaduan";
+import MainLayout from "./layouts/MainLayout";
+import PrivateRoute from "./utils/PrivateRoute";
 
 function App() {
   return (
@@ -13,11 +14,14 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route element={<MainLayout />}>
-          <Route path="/pengaduan/:id" element={<DetailPengaduan />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/pengaduan" element={<ListPengaduan />} />
-          <Route path="/pengaduan/tambah" element={<FormPengaduan />} />
+
+        <Route element={<PrivateRoute />}>
+          <Route element={<MainLayout />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/pengaduan" element={<ListPengaduan />} />
+            <Route path="/pengaduan/tambah" element={<FormPengaduan />} />
+            <Route path="/pengaduan/:id" element={<DetailPengaduan />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
